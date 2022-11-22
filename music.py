534d80d8c25d1f6e24d3a09a7f6c0da2e8d70002
Twan scheffers Scheffers <20155646@student.hhs.nl>
2022-11-22 21:57:54 +0100

    Path selection

--- music.py
+++ music.py
@@ -3,22 +3,21 @@ from pytube import YouTube
 from tkinter import *
 from tkinter import filedialog as fd
 
-#functie die een bestand opent
-def openFile():
+def openFolder():
+    #function for opening folder path for download location
     global folder_path
     pathname = fd.askdirectory()
     folder_path.set(pathname)
     print(folder_path.get())
 
 def myClick():
-    #maak een functie die de download start en de link pakt uit de entry e
+    #function for downloading MP3
     yt = YouTube(e.get())
     video = yt.streams.filter(only_audio=True).first()
     out_file = video.download(output_path=folder_path.get())
     base, ext = os.path.splitext(out_file)
     new_file = base + '.mp3'
     os.rename(out_file, new_file)
-
 
 root = Tk()
 root.geometry("400x160")
@@ -27,10 +26,8 @@ e = Entry(root, width=50, borderwidth=2)
 e.pack()
 
 folder_path = StringVar()
-#test commit de laatste poging vandaag
 myCanvas = Canvas(root, height=100, width=400, bg="#FFFFFF").pack()
 downloadButton = Button(root, text = "Download", width=24, command=myClick).place(x=1, y=127)
-pathButton = Button(root, text="Browse path", width=24, command=openFile).place(x=200, y=127)
-
+pathButton = Button(root, text="Browse path", width=24, command=openFolder).place(x=200, y=127)
 
 root.mainloop()
